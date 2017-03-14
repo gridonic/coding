065bf90b0889e4aba9e3f12608dea9bdcb5afdde
Sebastian Prein <basti@gridonic.ch>
2017-03-14 07:47:12 +0100

    Remove grunt folder as it’s in its own repository now

--- js/grunt/eslint.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    all: [
-        '<%= gruntDir %>/**/*.js'
-    ]
-};
--- js/grunt/shell.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    hello: 'echo "Hello"; exit 0',
-    hai: 'echo "Hai!"; exit 0'
-};
